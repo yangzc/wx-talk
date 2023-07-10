7f46efc2a921803fc718c3219bf2f78c13aa4199
yangzc <zhc_yang@163.com>
2023-07-10 23:02:24 +0800

    '修改包路径'

---
+++ components/msg-item/msg-item-base/index.js
@@ -0,0 +1,41 @@
+// components/msg-item-base/index.js
+Component({
+  options: {
+    multipleSlots: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    "align": {
+      type: String,
+      value: "left"
+    },
+    "name": {
+      type: String,
+      value: "十里桂花"
+    },
+    "head_src": {
+      type: String,
+      value: "https://res.wx.qq.com/mmbizwxaopennodelogicsvr_node/dist/images/Group%206_230f61.png"
+    },
+    "has_background": {
+      type: Boolean,
+      value: true
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/msg-item/msg-item-text/index.js
@@ -0,0 +1,38 @@
+// components/msg-item-text/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    "align": {
+      type: String,
+      value: "left"
+    },
+    "name": {
+      type: String,
+      value: "十里桂花"
+    },
+    "head_src": {
+      type: String,
+      value: "https://img0.baidu.com/it/u=1752903520,1572100722&fm=253&fmt=auto&app=138&f=JPEG?w=521&h=500"
+    },
+    "text": {
+      type: String,
+      value: ""
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
